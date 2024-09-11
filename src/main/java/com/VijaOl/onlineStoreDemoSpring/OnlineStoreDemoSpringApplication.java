d57bd9352ec794657f9bc67ca60d2b9bbffb2edc
Dm&try <dmitrypanyukov@gmail.com>
2024-09-12 00:32:33 +0300

    Начало

---
+++ src/main/java/com/VijaOl/onlineStoreDemoSpring/OnlineStoreDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.VijaOl.onlineStoreDemoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreDemoSpringApplication.class, args);
+	}
+
+}
